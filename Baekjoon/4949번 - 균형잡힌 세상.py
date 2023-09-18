e3ad37746c3e05cd2c6326d78fefce8f81c787e5
suyun329 <56281988+suyun329@users.noreply.github.com>
2023-09-18 21:55:02 +0900

    py commit

--- Baekjoon/4949번 - 균형잡힌 세상.py
+++ Baekjoon/4949번 - 균형잡힌 세상.py
@@ -1,36 +1,30 @@
-import sys
-input = sys.stdin.readline
-
 while True:
     T = input()
     s2 = []
     if T == '.':
         break
+        
+    for j in T:
+        if j=='(' or j=='[':
+            s2.append(j)
+                
+        #)    
+        elif j==')':
+            if s2 and s2[-1]=='(':
+                s2.pop()
+            else:
+                s2=[')']
+                break
+        #]        
+        elif j==']':
+            if s2 and s2[-1]=='[':
+                s2.pop()
+            else:
+                s2=[']']
+                break
+                
+    if s2:
+           print('no')
     else:
-        for j in T:
-            #( or [
-            if j=='(' or j=='[':
-                s2.append(j)
-                
-            #)    
-            elif j==')':
-                if not s2 or s2[-1]=='[':
-                    s2=[')']
-                    break
-                else:
-                    s2.pop()
-            #]        
-            elif j==']':
-                if s2 or s2[-1]=='(':
-                    s2=[']']
-                    break
-                else:
-                    s2.pop()
-                
-        if s2:
-            print('NO')
-        else:
-            print('YES')
+           print('yes')
 
-
-#state: 런타임 에러
