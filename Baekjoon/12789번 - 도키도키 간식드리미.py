6c7ddf2418fe83bbdece6d959bf16bdf7a3c0cd0
suyun329 <56281988+suyun329@users.noreply.github.com>
2023-09-20 20:59:19 +0900

    py commit

--- Baekjoon/12789번 - 도키도키 간식드리미.py
+++ Baekjoon/12789번 - 도키도키 간식드리미.py
@@ -3,6 +3,35 @@ import sys
 n = int(input())
 a = list(map(int, input().split()))
 b = []
+target = 1
+while a:
+    a1 = a
+    
+    if a1[0] == target:
+        a.pop(0)
+        target+=1
+    else:
+        if b and b[-1] == target:
+            b.pop(-1)
+            target+=1
+        else:
+            b.append(a[0])
+            a.pop(0)
+            
+if not b or b == sorted(b, reverse=True):
+    print('Nice')
+else:
+    print('Sad')
+        
+
+###시행착오
+##for -> while, b 스택에 target 있을 경우 추가
+
+import sys
+
+n = int(input())
+a = list(map(int, input().split()))
+b = []
 
 for j in range(n):
     a1 = a
