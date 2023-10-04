531e4b6b09a31dc2049f4b5d4e63e31808319dc1
suyun329 <56281988+suyun329@users.noreply.github.com>
2023-10-04 19:54:26 +0900

    py commit

--- Baekjoon/1463번 - 1로 만들기.py
+++ Baekjoon/1463번 - 1로 만들기.py
@@ -1,14 +1,11 @@
 n = int(input())
-a=0
-while n!=1:
-    a += 1
-    if n%3 == 0:
-        n = n//3
-        print('1', n)
-    elif n%2 == 0:
-        n = n//2
-        print('2',n)
-    else:
-        n= n-1
-        print('3',n)
-print(a)
+a=[0]*(n+1)
+for i in range(2, n+1):
+    a[i] = a[i-1]+1
+    if i%2==0:
+        a[i] = min(a[i], a[i//2]+1)
+    if i%3==0:
+        a[i] = min(a[i], a[i//3]+1)
+print(a[n])
+
+# 참고 - https://bio-info.tistory.com/159, https://seongonion.tistory.com/40
