095a159b856df6d12452a9e46df76ac54a35969d
suyun329 <56281988+suyun329@users.noreply.github.com>
2023-10-04 20:11:44 +0900

    py ing

--- Baekjoon/2156번 - 포도주 시식.py
+++ Baekjoon/2156번 - 포도주 시식.py
@@ -1,8 +1,14 @@
 n = int(input())
 w = []
+dp = [0]*(n+1)
 for _ in range(n):
     w.append(int(input()))
+dp[1] = w[0]
+if n >= 2:
+    dp[2] += w[1]
+    for i in range(3, n+1):
+        dp[i] = max()
 
-for i in range(n):
-    a = w[i]
-print(w)
+print(dp[n])
+
+# 참고 - https://bio-info.tistory.com/156
