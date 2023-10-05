429cb1bc9ebe8fdb80aeb14b45ca774046a2e025
suyun329 <56281988+suyun329@users.noreply.github.com>
2023-10-05 19:17:49 +0900

    py commit

--- Baekjoon/2156번 - 포도주 시식.py
+++ Baekjoon/2156번 - 포도주 시식.py
@@ -1,13 +1,13 @@
 n = int(input())
-w = []
 dp = [0]*(n+1)
+w = []
 for _ in range(n):
     w.append(int(input()))
 dp[1] = w[0]
 if n >= 2:
-    dp[2] += w[1]
+    dp[2] = w[0]+w[1]  # dp[2]+=w[1] (X)
     for i in range(3, n+1):
-        dp[i] = max()
+        dp[i] = max(dp[i-3]+w[i-2]+w[i-1], dp[i-2]+w[i-1], dp[i-1]) #sum(w[i-2:i] == w[i-2]+w[i-1]
 
 print(dp[n])
 
