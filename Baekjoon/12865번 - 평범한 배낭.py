a0ba15da85bd676d7c9415d094772f570a081cc7
suyun329 <56281988+suyun329@users.noreply.github.com>
2023-10-06 19:07:36 +0900

    py ing

---
+++ Baekjoon/12865번 - 평범한 배낭.py
@@ -0,0 +1,19 @@
+n, k = map(int, input().split())
+w, v, sum = [], [], []
+nk = 0
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    w.append(a)
+    v.append(b)
+w.append(0)
+i=0
+
+for i in range(n):
+    nk += w[i]
+    print(nk)
+    if nk <= k and nk+w[i+1] > k:
+        sum.append(nk)
+        nk = 0
+            
+print(sum)
