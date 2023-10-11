5c887301ddc8858a87b845f6650be83b4ea427d4
suyun329 <56281988+suyun329@users.noreply.github.com>
2023-10-11 21:17:26 +0900

    py ing

---
+++ Baekjoon/10816번 - 숫자 카드 2.py
@@ -0,0 +1,13 @@
+
+# 시행착오 - 시간초과
+
+n = int(input())
+a, b, r = [], [], []
+a = list(map(int, input().split()))
+m = int(input())
+b = list(map(int, input().split()))
+
+for i in b:
+    r.append(a.count(i))
+
+print(*r, sep=' ')
